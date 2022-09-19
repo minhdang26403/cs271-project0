4ba0045f191be30aa8154170110a24cc066c4a73
Dang Minh Truong <truongminhdang26403@gmail.com>
2022-09-19 15:07:09 -0400

    Create new folder

---
+++ project1/set.cpp
@@ -0,0 +1,195 @@
+#include "set.h"
+
+template<typename ValueType>
+Set<ValueType>::Set(const Set &src) {
+  deep_copy(src);
+}
+
+template<typename ValueType>
+Set<ValueType>::~Set() {
+  Node *current = head_;
+  Node *node_to_delete;
+  // Goes through each element of the Set and deletes it
+  while (current != nullptr) {
+    node_to_delete = current;
+    current = current->next_;
+    delete node_to_delete;
+  }
+}
+
+template<typename ValueType>
+Set<ValueType>& Set<ValueType>::operator=(const Set &src) {
+  // Copy and swap idiom
+  Set copy(src);
+  copy.swap(*this);
+  return *this;
+}
+
+template<typename ValueType>
+void Set<ValueType>::insert(const ValueType &x) {
+  if (contains(x)) {
+    return;
+  }
+  // Inserts a new node if it doesn't exist
+  auto new_node = new Node(x);
+  new_node->next_ = head_;
+  head_ = new_node;
+  ++size_;
+}
+
+template<typename ValueType>
+void Set<ValueType>::remove(const ValueType &x) {
+  Node *prev = nullptr;
+  Node *node_to_delete = head_;
+  // Finds the element to remove and keeps a pointer to the previous node
+  // of that element
+  while (node_to_delete != nullptr && node_to_delete->value_ != x) {
+    prev = node_to_delete;
+    node_to_delete = node_to_delete->next_;
+  }
+
+  // Does nothing if that element doesn't exist
+  if (node_to_delete == nullptr) {
+    return;
+  }
+
+  if (prev == nullptr) {
+    // The element we want to remove is the head
+    head_ = head_->next_;
+  } else {
+    prev->next_ = node_to_delete->next_;
+  }
+  delete node_to_delete;
+  --size_;
+}
+
+template<typename ValueType>
+bool Set<ValueType>::contains(const ValueType &x) const {
+  Node *current = head_;
+  // Goes through each node to check if it contains the element with value `x`
+  while (current != nullptr) {
+    if (current->value_ == x) {
+      return true;
+    }
+    current = current->next_;
+  }
+  return false;
+}
+
+template<typename ValueType>
+bool Set<ValueType>::operator==(const Set &t) const {
+  // two sets are equal if they are subsets of each other
+  return *this <= t && t <= *this;
+}
+
+template<typename ValueType>
+bool Set<ValueType>::operator<=(const Set &t) const {
+  Node *current = head_;
+  // Goes through each element in the first set to make sure it 
+  // exists in the second set
+  while (current != nullptr) {
+    if (!t.contains(current->value_)) {
+      return false;
+    }
+    current = current->next_;
+  }
+  return true;
+}
+
+template<typename ValueType>
+Set<ValueType> Set<ValueType>::operator+(const Set &t) {
+  Set<ValueType> result;
+  Node *t_element = t.begin();
+  // Inserts all elements from set `t` first
+  while (t_element != nullptr) {
+    result.insert(t_element->value_);
+    t_element = t_element->next_;
+  }
+
+  // assume `this` object is set `s`
+  Node *s_element = head_;
+  // Inserts all elements from set `s` (insert method will skip
+  // duplicate values)
+  while (s_element != nullptr) {
+    result.insert(s_element->value_);
+    s_element = s_element->next_;
+  }
+
+  return result;
+}
+
+template<typename ValueType>
+Set<ValueType> Set<ValueType>::operator&(const Set &t) {
+  Set<ValueType> result;
+  // assume `this` object is set `s`
+  Node *s_element = head_;
+  // Goes through each element in set `s` and inserts it into
+  // the `result` set if it also exists in set `t`
+  while (s_element != nullptr) {
+    if (t.contains(s_element->value_)) {
+      result.insert(s_element->value_);
+    }
+    s_element = s_element->next_;
+  }
+  return result;
+}
+
+template<typename ValueType>
+Set<ValueType> Set<ValueType>::operator-(const Set &t) {
+  Set<ValueType> result;
+  Node *s_element = head_;
+  // Goes through each element in set `s` and inserts it into
+  // the `result` set if it doesn't exist in set `t`
+  while (s_element != nullptr) {
+    if (!t.contains(s_element->value_)) {
+      result.insert(s_element->value_);
+    }
+    s_element = s_element->next_;
+  }
+  return result;
+}
+
+template<typename ValueType>
+std::string Set<ValueType>::to_string() {
+  Node *current = head_;
+  // Empty set
+  if (current == nullptr) {
+    return "";
+  }
+  std::stringstream set_str;
+  // Goes through each element except the last element and inserts 
+  // its value into the string stream
+  while (current->next_ != nullptr) {
+    set_str << current->value_ << " ";
+    current = current->next_;
+  }
+  set_str << current->value_;
+  return set_str.str();
+}
+
+template<typename ValueType>
+void Set<ValueType>::deep_copy(const Set &src) {
+  if (src.head_ == nullptr) {
+    // The source set is empty
+    head_ = nullptr;
+  } else {
+    // Create the head of the linked list first
+    Node *src_node = src.head_;
+    head_ = new Node(src_node->value_);
+
+    // Traverse through the source set to copy element from
+    Node *current = head_;
+    src_node = src_node->next_;
+    for (; src_node != nullptr; src_node = src_node->next_) {
+      current->next_ = new Node(src_node->value_);
+      current = current->next_;
+    }
+  }
+  size_ = src.size_;
+}
+
+template<typename ValueType>
+void Set<ValueType>::swap(const Set &rhs) {
+  std::swap(head_, rhs.head_);
+  std::swap(size_, rhs.size_);
+}
---
+++ project1/test_set.cpp
@@ -0,0 +1,469 @@
+//  test_set.cpp
+//  CS 271 Set Project: Unit Test File
+#include "set.h"
+
+#include <iostream>
+#include <string>
+
+
+void test_insert() {
+  try {
+    Set<std::string> s;
+    std::string set_str = s.to_string();
+    if (set_str != "") {
+      std::cout << "Incorrect insert result. Expected \"\" but got: " << set_str << '\n';
+    }
+
+    s.insert("abc");
+    set_str = s.to_string();
+    if (set_str != "abc") {
+      std::cout << "Incorrect insert result. Expected abc but got: " << set_str << '\n';
+    }
+
+    s.insert("def");
+    s.insert("ghi");
+    s.insert("def");
+
+    // The set doesn't accept duplicate values
+    set_str = s.to_string();
+    if (set_str != "ghi def abc") {
+      std::cout << "Incorrect insert result. Expected \"ghi def abc\" but got: " << set_str << '\n';
+    }
+
+    // Try to insert an empty string
+    s.insert("");
+    set_str = s.to_string();
+    if (set_str != " ghi def abc") {
+      std::cout << "Incorrect insert result. Expected \" ghi def abc\" but got: " << set_str << '\n';
+    }
+
+  } catch (std::exception &e){
+    std::cerr << "Error inserting into set : " << e.what() << std::endl;
+  }
+}
+
+void test_remove() {
+  try {
+    Set<int> s;
+    // The set remains unchanged when trying to remove a non-existing element
+    s.remove(1);
+    std::string set_str = s.to_string();
+    if (s.to_string() != "") {
+        std::cout << "Incorrect remove result. Expected but got : " << set_str << std::endl;
+    }
+
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+    s.insert(4);
+    s.insert(5);
+
+    s.remove(6);
+    set_str = s.to_string();
+    if (s.to_string() != "5 4 3 2 1") {
+      std::cout << "Incorrect remove result. Expected 5 4 3 2 1 but got : " << set_str << std::endl;
+    }
+
+    // successfully removes the element at the beginning
+    s.remove(5);
+    set_str = s.to_string();
+    if (s.to_string() != "4 3 2 1") {
+      std::cout << "Incorrect remove result. Expected 4 3 2 1 but got : " << set_str << std::endl;
+    }
+
+    // successfully removes the element in the middle
+    s.remove(3);
+    set_str = s.to_string();
+    if (s.to_string() != "4 2 1") {
+      std::cout << "Incorrect remove result. Expected 4 2 1 but got : " << set_str << std::endl;
+    }
+
+    // successfully removes the element at the end
+    s.remove(1);
+    set_str = s.to_string();
+    if (s.to_string() != "4 2") {
+      std::cout << "Incorrect remove result. Expected 4 2 but got : " << set_str << std::endl;
+    }
+  } catch (std::exception &e) {
+    std::cerr << "Error removing from set : " << e.what() << std::endl;
+  }
+}
+
+void test_cardinality() {
+  try {
+    Set<char> s;
+    int c = s.cardinality();
+
+    // An empty set has the cardinality 0
+    if(c != 0) {
+        std::cout << "Incorrect cardinality. Expected 0 but got : " << c << std::endl;
+    }
+
+    s.insert('a');
+    s.insert('b');
+    s.insert('c');
+    s.insert('d');
+
+    c = s.cardinality();
+    if(c != 4) {
+        std::cout << "Incorrect cardinality. Expected 4 but got : " << c << std::endl;
+    }
+
+    // if an element is removed, the cardinality of the set decrements by 1
+    s.remove('a');
+    s.remove('d');
+    c = s.cardinality();
+    if(c != 2) {
+        std::cout << "Incorrect cardinality. Expected 2 but got : " << c << std::endl;
+    }
+
+    // if an element is inserted, the cardinality of the set increments by 1
+    s.insert('e');
+    s.insert('w');
+    s.insert('o');
+    c = s.cardinality();
+    if (c != 5) {
+        std::cout << "Incorrect cardinality. Expected 5 but got : " << c << std::endl;
+    }
+
+    // if we try to insert an existing element, the cardinality remains unchanged
+    s.insert('b');
+    c = s.cardinality();
+    if (c != 5) {
+        std::cout << "Incorrect cardinality. Expected 5 but got : " << c << std::endl;
+    }
+  } catch (std::exception &e) {
+      std::cerr << "Error in determining set cardinality : " << e.what() << std::endl;
+  }
+}
+
+void test_empty() {
+  try {
+    Set<float> s;
+    if(!s.empty()){
+      std::cout << "Empty set identified as non-empty" << std::endl;
+    }
+
+    s.insert(1.2);
+    s.insert(2.4);
+    if(s.empty()) {
+      std::cout << "Non-empty set identified as empty" << std::endl;
+    }
+
+    // Remove non-exist element
+    s.remove(1.4);
+    if (s.empty()) {
+      std::cout << "Non-empty set identified as empty" << std::endl;
+    }
+
+    s.remove(1.2);
+    if(s.empty()) {
+      std::cout << "Non-empty set identified as empty" << std::endl;
+    }
+
+    s.remove(2.4);
+    if(!s.empty()) {
+      std::cout << "Empty set identified as non-empty" << std::endl;
+    }
+  } catch (std::exception &e) {
+    std::cerr << "Error in determining if set is the empty set : " << e.what() << std::endl;
+  }
+}
+
+void test_contains() {
+  try {
+    Set<std::string> s;
+
+    // an empty set does not contain any element
+    if(s.contains("good")) {
+        std::cout << "Incorrectly identified good as in the set " << s.to_string() << std::endl;
+    }
+
+    s.insert("hello");
+    if (!s.contains("hello")) {
+        std::cout << "incorrectly identified hello as in the set " << s.to_string() << std::endl;
+    }
+
+    s.insert("abc");
+    s.insert("def");
+    s.insert("ghi");
+    if(s.contains("iop")) {
+        std::cout << "Incorrectly identified iop as in the set " << s.to_string() << std::endl;
+    }
+    if(!s.contains("ghi")) {
+        std::cout << "Incorrectly identified ghi as NOT in the set " << s.to_string() << std::endl;
+    }
+
+    // successfully identifies the element that was removed
+    s.remove("def");
+    if (s.contains("def")) {
+        std::cout << "Incorrectly identified def as in the set " << s.to_string() << std::endl;
+    }
+  } catch (std::exception &e) {
+      std::cerr << "Error in determining if value contained in se : " << e.what() << std::endl;
+  }
+}
+
+void test_equality() {
+  try {
+    Set<int> s;
+    Set<int> t;
+
+    // Two empty sets are equal
+    if(!(s==t)) {
+      std::cout << "Empty sets should be considered equal" << std::endl;
+    }
+    s.insert(4);
+    s.insert(5);
+    s.insert(6);
+    s.insert(7);
+    if(s==t) {
+      std::cout << "Incorrectly identified set " << s.to_string() << " as equivalent to " << t.to_string() << std::endl;
+    }
+
+    t.insert(7);
+    t.insert(6);
+    t.insert(5);
+    t.insert(4);
+    if(!(s==t)) {
+      std::cout << "Sets " << s.to_string() << " and " << t.to_string() << " should be considered equal." << std::endl;
+    }
+
+    // after removing an element from one set, it is no longer equal to the other set
+    s.remove(5);
+    if(s==t) {
+      std::cout << "Incorrectly identified set " << s.to_string() << " as equivalent to " << t.to_string() << std::endl;
+    }
+
+    // after removing the same element from both sets that are equal, the sets remain equal
+    t.remove(5);
+    if (!(s==t)) {
+      std::cout << "Sets " << s.to_string() << " and " << t.to_string() << " should be considered equal." << std::endl;
+    }
+  } catch (std::exception &e) {
+    std::cerr << "Error in determining set equality : " << e.what() << std::endl;
+  }
+}
+
+void test_subset() {
+  try {
+    Set<std::string> s;
+    Set<std::string> t;
+
+    // an empty set is a subset of an empty set
+    if(!(s<=t)) {
+      std::cout << "Set " << s.to_string() << " should be considered a subset of the set " << t.to_string() << std::endl;
+    }
+
+    t.insert("abc");
+    t.insert("def");
+
+    // an empty set is a subset of all sets
+    if(!(s<=t)) {
+      std::cout << "Set " << s.to_string() << " should be considered a subset of the set " << t.to_string() << std::endl;
+    }
+
+    // Equal sets are subsets of each other
+    s.insert("def");
+    s.insert("abc");
+    if(!(s<=t)) {
+      std::cout << "Set " << s.to_string() << " should be considered a subset of the set " << t.to_string() << std::endl;
+    }
+
+    t.insert("ghi");
+    if(!(s<=t)) {
+      std::cout << "Set " << s.to_string() << " should be considered a subset of the set " << t.to_string() << std::endl;
+    }
+
+    s.insert("jkl");
+    if(s<=t) {
+      std::cout << "Set " << s.to_string() << " should NOT be considered a subset of the set " << t.to_string() << std::endl;
+    }
+
+    s.remove("jkl");
+    s.remove("def");
+    if (!(s<=t)) {
+      std::cout << "Set " << s.to_string() << " should be considered a subset of the set " << t.to_string() << std::endl;
+    }
+
+    // the only subset of an empty set is another empty set
+    t.remove("ghi");
+    t.remove("abc");
+    t.remove("def");
+    if ((s<=t)) {
+      std::cout << "Set " << s.to_string() << " should NOT be considered a subset of the set " << t.to_string() << std::endl;
+    }
+  } catch (std::exception& e) {
+    std::cerr << "Error in determining subset : " << e.what() << std::endl;
+  }
+}
+
+void test_union() {
+  try {
+    Set<int> s;
+    Set<int> t;
+
+    // union of empty sets is an empty set
+    std::string set_str = (s+t).to_string();
+    if(set_str != "") {
+      std::cout << "Union of the empty sets should be the empty set. Instead got : " << set_str << std::endl;
+    }
+
+    s.insert(1);
+    t.insert(1);
+    set_str = (s+t).to_string();
+    if(set_str != "1") {
+      std::cout << "Union of " << s.to_string() << " and " << t.to_string() << " was expected to be 1 but instead got : " << set_str << std::endl;
+    }
+
+    t.insert(2);
+    t.insert(3);
+    set_str = (s+t).to_string();
+    if(set_str != "1 2 3") {
+      std::cout << "Union of " << s.to_string() << " and " << t.to_string() << " was expected to be 1 2 3 but instead got : " << set_str << std::endl;
+    }
+
+    s.insert(4);
+    s.insert(5);
+    set_str = (s+t).to_string();
+    if(set_str != "4 5 1 2 3") {
+      std::cout << "Union of " << s.to_string() << " and " << t.to_string() << " was expected to be 4 5 1 2 3 but instead got : " << set_str << std::endl;
+    }
+
+    s.remove(4);
+    s.remove(5);
+    t.remove(2);
+    t.remove(3);
+    s.insert(7);
+    t.insert(7);
+
+    // if two sets are equal, the union set is also equal
+    set_str = (s+t).to_string();
+    if(set_str != "1 7" && !(s==t)) {
+      std::cout << "Union of " << s.to_string() << " and " << t.to_string() << " was expected to be 1 7 but instead got : " << set_str << std::endl;
+    }
+  } catch (std::exception& e) {
+    std::cerr << "Error in generating the union set : " << e.what() << std::endl;
+  }
+}
+
+void test_intersection() {
+  try {
+    Set<char> s;
+    Set<char> t;
+    std::string set_str = (s&t).to_string();
+
+    // intersection of empty sets is an empty set
+    if(set_str != "") {
+      std::cout << "Intersection of the empty sets should be the empty set. Instead got : " << set_str << std::endl;
+    }
+
+    s.insert('7');
+    s.insert('8');
+    t.insert('9');
+
+    // if one of the sets is empty, then the intersection set is also empty
+    set_str = (s&t).to_string();
+    if(set_str != "") {
+      std::cout << "Intersection of " << s.to_string() << " and " << t.to_string() << " was expected to be the empty set but instead got : " << set_str << std::endl;
+    }
+
+    // if none of the elements from the two sets overlap, then the intersection set is empty
+    t.insert('5');
+    t.insert('6');
+    set_str = (s&t).to_string();
+    if(set_str != "") {
+      std::cout << "Intersection of " << s.to_string() << " and " << t.to_string() << " was expected to be the empty set but instead got : " << set_str << std::endl;
+    }
+
+    t.insert('8');
+    set_str = (s&t).to_string();
+    if(set_str != "8") {
+      std::cout << "Intersection of " << s.to_string() << " and " << t.to_string() << " was expected to be 8 but instead got : " << set_str << std::endl;
+    }
+
+    t.remove('5');
+    t.remove('6');
+    t.remove('9');
+    t.insert('7');
+    set_str = (s&t).to_string();
+
+    // if two sets are equal, the intersection set is also equal
+    if (!(s==t)) {
+      std::cout << "Sets " << s.to_string() << " and " << t.to_string() << " are not equal." << std::endl;
+    }
+
+    if(set_str != "7 8") {
+      std::cout << "Intersection of " << s.to_string() << " and " << t.to_string() << " was expected to be 7 8 9 but instead got : " << set_str << std::endl;
+    }
+  } catch (std::exception& e) {
+    std::cerr << "Error in generating the intersection set : " << e.what() << std::endl;
+  }
+}
+
+void test_diff() {
+  try {
+    Set<int> s;
+    Set<int> t;
+    std::string set_str = (s-t).to_string();
+
+    // difference of empty sets is an empty set
+    if(set_str != "") {
+      std::cout << "Difference of the empty sets should be the empty set. Instead got : " << set_str << std::endl;
+    }
+
+    s.insert(5);
+    s.insert(6);
+    set_str = (s-t).to_string();
+
+    // difference of a non-empty set and an empty set is just the non-empty set
+    if(set_str != "5 6") {
+      std::cout << "Set " << s.to_string() << " \\ Set " << t.to_string() << " was expected to be 5 6 set but instead got : " << set_str << std::endl;
+    }
+
+    // difference of an empty set and a non-empty set is an empty set
+    set_str = (t-s).to_string();
+    if (set_str != "") {
+      std::cout << "Set " << s.to_string() << " \\ Set " << t.to_string() << " was expected to be the empty set but instead got : " << set_str << std::endl;
+    }
+
+    // if the two sets are equal, their difference is an empty set
+    t.insert(6);
+    t.insert(5);
+    set_str = (s-t).to_string();
+    if(set_str != "") {
+      std::cout << "Set " << s.to_string() << " \\ Set " << t.to_string() << " was expected to be the empty set but instead got : " << set_str << std::endl;
+    }
+
+    s.insert(7);
+    s.insert(8);
+    set_str = (s-t).to_string();
+    if(set_str != "7 8") {
+      std::cout << "Set " << s.to_string() << " \\ Set " << t.to_string() << " was expected to be 7 8 set but instead got : " << set_str << std::endl;
+    }
+
+  } catch (std::exception& e) {
+    std::cerr << "Error in generating the intersection set : " << e.what() << std::endl;
+  }
+}
+
+int main() {
+  test_insert();
+  test_remove();
+  test_cardinality();
+  test_empty();
+  test_contains();
+  test_equality();
+  test_subset();
+  test_union();
+  test_intersection();
+  test_diff();
+
+  std::cout << "Testing completed" << std::endl;
+    
+  return 0;
+}
+
+
+
+
